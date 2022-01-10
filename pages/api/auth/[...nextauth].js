053f5e64d56371ccc901bf0a1cda09b06d837895
Faunbox <faunbox2@gmail.com>
2022-01-10 22:49:55 +0100

    fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -24,7 +24,7 @@ export default NextAuth({
   },
   callbacks: {
     async session({ session, user, token }) {
-      session.role = user.role;
+      session.user.role = user.role;
       return session;
     },
   },
