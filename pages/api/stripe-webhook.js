9f1068cf9fe222060036cd3e41af5799a3db8155
Faunbox <faunbox2@gmail.com>
2022-01-14 16:48:33 +0100

    webhook test

--- pages/api/stripe-webhook.js
+++ pages/api/stripe-webhook.js
@@ -12,28 +12,28 @@ const handler = async (req, res) => {
   const reqBuffer = await buffer(req);
 
   let event;
-  if (req.method === "POST") {
-    console.log("signature", signature);
-    console.log("endpoint", endpointSecret);
-    console.log("req", req);
-    try {
-      event = stripe.webhooks.constructEvent(
-        reqBuffer,
-        signature,
-        endpointSecret
-      );
-    } catch (error) {
-      console.log(error);
-      res.status(400).send(`Webhook error: ${error.message}`);
-      return;
-    }
-  } else {
-    res.status(405).end("Method not allowed");
+  //   if (req.method === "POST") {
+  console.log("signature", signature);
+  console.log("endpoint", endpointSecret);
+  console.log("req", req);
+  try {
+    event = stripe.webhooks.constructEvent(
+      reqBuffer,
+      signature,
+      endpointSecret
+    );
+    console.log("event w srodku try", { event });
+    return res.status(200).json(event);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(`Webhook error: ${error.message}`);
+    return;
   }
+  //   } else {
+  //     res.status(405).end("Method not allowed");
+  //   }
 
-  console.log({ event });
-
-  res.json({ received: true });
+  //   console.log("event za blokiem try/catch", { event });
 };
 
 export default handler;
