14da94391108f6ac4ebe7ca1b95a9841bd0c6131
Faunbox <faunbox2@gmail.com>
2022-01-15 11:41:39 +0100

    test

--- pages/zamowienie/podsumowanie.js
+++ pages/zamowienie/podsumowanie.js
@@ -1,19 +1,22 @@
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 import { useEffect } from "react";
 
 const FinishingOrder = () => {
   const getDataFromWebhook = async () => {
-    const req = await fetch("/api/stripe-webhook", { method: "POST" });
+    const req = await fetch("/api/stripe-webhook", { method: "GET" });
     const data = await req.json();
     console.log("dane z webhooka", data);
   };
 
   useEffect(() => {
     getDataFromWebhook();
-    return getDataFromWebhook;
   }, []);
 
   return <p>gitara</p>;
 };
 
-export default dynamic(async () => FinishingOrder, { ssr: false });
+export default FinishingOrder;
+
+// export async function getStaticProps(req, res) {
+
+// }
