5e9e0615f472d206833548d75b1943dc363ea848
Faunbox <faunbox2@gmail.com>
2022-01-15 11:37:34 +0100

    mdb

--- db/mongodb.js
+++ db/mongodb.js
@@ -6,10 +6,10 @@ const options = {
   useNewUrlParser: true,
 };
 
+export let client;
 let clientPromise;
 
 client = new MongoClient(uri, options, () => console.log("Połączono"));
-console.log("MongoClient", MongoClient.length);
 clientPromise = client.connect();
 
 // Export a module-scoped MongoClient promise. By doing this in a
